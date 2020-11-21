093ebb6ee956bce855bac7df5170d90c9825fa19
yanghaku <1961882079@qq.com>
2020-11-21 20:31:33 +0800

    1112

--- DataInit.py
+++ DataInit.py
@@ -18,24 +18,22 @@ class Normal01:
 
 
 class DataManager:
-    def __init__(self, data_file, label_file, save_data, save_label):
+    def __init__(self, data_file, label_file, save_data, save_label, L, R):
         print("data ", data_file, "init...")
 
         data_dim = 235
         self.norm01 = Normal01(data_dim)
 
-        # # 读取label文件
-        # f_label = open(label_file, "r", encoding='utf-8')
-        # self.label = []
-        # for row in f_label:
-        #     x, y = row.strip().split(',')
-        #     self.label.append(int(y))
-        # f_label.close()
-        #
-        # np.save("./data/" + save_label, np.array(self.label))
+        # 读取label文件
+        f_label = open(label_file, "r", encoding='utf-8')
+        self.label = []
+        for row in f_label:
+            x, y = row.strip().split(',')
+            self.label.append(int(y))
+        f_label.close()
 
         self.fe = FE(data_file, np.inf)
-        features = []
+        self.features = []
 
         x = 0
         while True:
@@ -44,34 +42,33 @@ class DataManager:
             if len(feature) == 0:
                 break
             feature = self.norm01.norm(np.array(feature))
-            features.append(feature)
+            self.features.append(feature)
 
             x += 1
             if x % 10000 == 0:
                 print(x)
 
-        ff = np.array(features)
-        print("features is : ", ff.shape)
-        np.save("./data/" + save_data, ff)
+        labels = np.array(self.label[L:R])
+        data = np.array(self.features[L:R])
+
+        # 打乱
+        indices = np.random.permutation(data.shape[0])
+        labels = labels[indices]
+        data = data[indices]
+        print("num: ", labels.shape[0], data.shape[0])
+        np.save(save_label, labels)
+        np.save(save_data, data)
 
         print(data_file + " data init success")
 
 
-#
-# d1 = DataManager("D:\\Dataset\\KITSUNE\\Mirai\\test.pcap.tsv", "E:\\dataset\\kitsune\\Mirai_labels.csv", "mirai.npy",
-#                  "mirai_label.npy")
-#
+# d1 = DataManager("D:\\Dataset\\KITSUNE\\Mirai\\test.pcap.tsv", "E:\\dataset\\kitsune\\Mirai_labels.csv",
+#                  "./data/mirai_10w.npy", "./data/mirai_label_10w.npy", 50000, 150000)
+# #
 # d2 = DataManager("D:\\Dataset\\KITSUNE\\ARP_MitM\\test.pcap.tsv", "E:\\dataset\\kitsune\\ARP_MitM_labels.csv",
-#                  "arp.npy", "arp_label.npy")
+#                  "./data/arp_10w.npy", "./data/arp_label_10w.npy", 1240000, 1340000)
 # d3 = DataManager("D:\\Dataset\\KITSUNE\\SSDP_Flood\\test.pcap.tsv", "E:\\dataset\\kitsune\\SSDP_Flood_labels.csv",
-#                  "ssdp_flood.npy", "ssdp_flood_label.npy")
-#
-# d4 = DataManager("D:\\Dataset\\KITSUNE\\Fuzzing\\test.pcap.tsv", "E:\\dataset\\kitsune\\Fuzzing_labels.csv",
-#                  "fuzzing.npy", "fuzzing_label.npy")
-
-d1 = DataManager("D:\\Dataset\\KITSUNE\\Mirai\\mirai_10w.tsv", "", "mirai_10w.npy", "")
-
-d2 = DataManager("D:\\Dataset\\KITSUNE\\ARP_MitM\\arp_10w.tsv", "", "arp_10w.npy", "")
-d3 = DataManager("D:\\Dataset\\KITSUNE\\SSDP_Flood\\ssdp_10w.tsv", "", "ssdp_10w.npy", "")
-
-d4 = DataManager("D:\\Dataset\\KITSUNE\\Fuzzing\\fuzzing_10w.tsv", "", "fuzzing_10w.npy", "")
+#                  "./data/ssdp_10w.npy", "./data/ssdp_label_10w.npy", 2560000, 2660000)
+# #
+d4 = DataManager("D:\\Dataset\\KITSUNE\\Fuzzing\\test.pcap.tsv", "E:\\dataset\\kitsune\\Fuzzing_labels.csv",
+                 "./data/fuzzing_10w.npy", "./data/fuzzing_label_10w.npy", 1250000, 1350000)
